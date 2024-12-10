e81ebc3053c9a3a6fa481b8947019a6e4486a3b0
Patrick Flaherty <patrickdanielflaherty@gmail.com>
2024-12-10 12:55:53 -0500

    update onclick

---
+++ assets/js/subscribe.js
@@ -0,0 +1,15 @@
+/*
+	Hyperspace by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+	/**
+	 * Generate an indented list of links from a nav. Meant for use with panel().
+	 * @return {jQuery} jQuery object.
+	 */
+	$.fn.updateSubscrbeButton = function() {
+		document.getElementById("subscribeButton").value = "Thank You!";
+	};
+})(jQuery);
