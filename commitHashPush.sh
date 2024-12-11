8ef96dd61abb6f1af93bfaf6b2488fd30de31e73
Patrick Flaherty <patrickdanielflaherty@gmail.com>
2024-12-11 09:26:17 -0500

    test out git commit hash update

---
+++ commitHashPush.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Check if a commit message is provided
+if [ -z "$1" ]; then
+  echo "Please provide a commit message."
+  exit 1
+fi
+
+rm version.txt
+git rev-parse HEAD > version.txt
+
+# Add all changes
+git add .
+
+# Commit changes with the provided message
+git commit -m "$1"
+
+# Push changes to the remote repository
+git push origin main
+
+echo "Changes committed and pushed to the main branch."
