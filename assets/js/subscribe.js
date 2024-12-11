655c58e492c985dadffa83d817574ad50f7fd81d
Patrick Flaherty <patrickdanielflaherty@gmail.com>
2024-12-11 09:11:00 -0500

    fix button update, but the POST may be broken now

--- assets/js/subscribe.js
+++ assets/js/subscribe.js
@@ -4,12 +4,10 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function($) {
-	/**
-	 * Generate an indented list of links from a nav. Meant for use with panel().
-	 * @return {jQuery} jQuery object.
-	 */
-	$.fn.updateSubscrbeButton = function() {
-		document.getElementById("subscribeButton").value = "Thank You!";
-	};
-})(jQuery);
+$(document).ready(function() {
+	$("#subscribeButton").click(function() {
+		// Code to execute when the button is clicked
+		console.log("Button clicked!");
+		$("#subscribeButton").attr('value', 'Done!');
+	});
+});
